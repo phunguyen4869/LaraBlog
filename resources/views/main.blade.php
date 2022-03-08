b399a9a45438bf08c8cc9ce6ec428bb90bc0dba3
phunguyen4869 <phunguyen4869@gmail.com>
2022-03-08 17:57:56 +0700

    first commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    @include('head')
+</head>
+
+<body>
+    <div class="fh5co-loader"></div>
+
+    <div id="page">
+
+        @include('nav')
+
+        @yield('content')
+
+        @include('footer')
+    </div>
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+    </div>
+</body>
+
+</html>
