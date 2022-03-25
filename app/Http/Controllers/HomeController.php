1f5f978ed80eeacc0978454b5fd86be3c8605be7
phunguyen4869 <phunguyen4869@gmail.com>
2022-03-25 11:14:57 +0700

    test git on PHP Storm

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,7 @@ class HomeController extends Controller
 
     public function index()
     {
+        echo 'hehe';
         $sliders = $this->slider->get();
 
         $categories = $this->category->getAll();
