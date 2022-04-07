649ffe34c45d42220b3af0401f4fe3a299036528
Phu Nguyen <phunguyen4869@gmail.com>
2022-04-07 18:11:51 +0700

    update bootstrap

--- resources/views/Admin/Post/list.blade.php
+++ resources/views/Admin/Post/list.blade.php
@@ -1,5 +1,10 @@
 @extends('Admin.main')
 
+@section('head')
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/v/bs5/jszip-2.5.0/dt-1.11.5/b-2.2.2/b-colvis-2.2.2/b-html5-2.2.2/b-print-2.2.2/date-1.1.2/fc-4.0.2/fh-3.2.2/r-2.2.9/rg-1.1.4/sc-2.0.5/sb-1.3.2/sl-1.3.4/datatables.min.css" />
+@endsection
+
 @section('content')
     <div class="col-md-12 align-items-right">
         <div class="card">
@@ -63,4 +68,16 @@
         </div>
         <!-- /.card -->
     </div>
+@endsection
+
+@section('footer')
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js">
+    </script>
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js">
+    </script>
+    <script type="text/javascript"
+        src="https://cdn.datatables.net/v/bs5/jszip-2.5.0/dt-1.11.5/b-2.2.2/b-colvis-2.2.2/b-html5-2.2.2/b-print-2.2.2/date-1.1.2/fc-4.0.2/fh-3.2.2/r-2.2.9/rg-1.1.4/sc-2.0.5/sb-1.3.2/sl-1.3.4/datatables.min.js">
+    </script>
 @endsection
